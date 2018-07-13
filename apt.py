fc488116c9213750bd4fb80f9b0ae6ffcd972078
Martin Bernardi (Laptop) <martinbernardi@openmailbox.org>
2018-07-13 18:13:28 -0300

    Align image to sync A frames

--- apt.py
+++ apt.py
@@ -22,8 +22,7 @@ class APT(object):
         filtered = scipy.signal.medfilt(numpy.abs(hilbert), 5)
         reshaped = filtered.reshape(len(filtered) // 5, 5)
         digitized = self._digitize(reshaped[:, 2])
-        lines = int(len(digitized) / self.NOAA_LINE_LENGTH)
-        matrix = digitized.reshape((lines, self.NOAA_LINE_LENGTH))
+        matrix = self._reshape(digitized)
         image = PIL.Image.fromarray(matrix)
         if not outfile is None:
             image.save(outfile)
@@ -31,12 +30,59 @@ class APT(object):
         return matrix
 
     def _digitize(self, signal, plow=0.5, phigh=99.5):
+        '''
+        Convert signal to numbers between 0 and 255.
+        '''
         (low, high) = numpy.percentile(signal, (plow, phigh))
         delta = high - low
         data = numpy.round(255 * (signal - low) / delta)
         data[data < 0] = 0
         data[data > 255] = 255
         return data.astype(numpy.uint8)
+
+    def _reshape(self, signal):
+        '''
+        Find sync frames and reshape the 1D signal into a 2D image.
+
+        Finds the sync A frame by looking at the maximum values of the cross
+        correlation between the signal and a hardcoded sync A frame.
+
+        The expected distance between sync A frames is 2080 samples, but with
+        small variations because of Doppler effect.
+        '''
+        # sync frame to find: seven impulses and some black pixels (some lines
+        # have something like 8 black pixels and then white ones)
+        syncA = [0, 128, 255, 128]*7 + [0]*7
+
+        # list of maximum correlations found: (index, value)
+        peaks = [(0, 0)]
+
+        # minimum distance between peaks
+        mindistance = 2000
+
+        # need to shift the values down to get meaningful correlation values
+        signalshifted = [x-128 for x in signal]
+        syncA = [x-128 for x in syncA]
+        for i in range(len(signal)-len(syncA)):
+            corr = numpy.dot(syncA, signalshifted[i : i+len(syncA)])
+
+            # if previous peak is too far, keep it and add this value to the
+            # list as a new peak
+            if i - peaks[-1][0] > mindistance:
+                peaks.append((i, corr))
+
+            # else if this value is bigger than the previous maximum, set this
+            # one
+            elif corr > peaks[-1][1]:
+                peaks[-1] = (i, corr)
+
+        # create image matrix starting each line on the peaks found
+        matrix = []
+        for i in range(len(peaks) - 1):
+            matrix.append(signal[peaks[i][0] : peaks[i][0] + 2080])
+
+        return numpy.array(matrix)
+
 
 if __name__ == '__main__':
     apt = APT(sys.argv[1])
